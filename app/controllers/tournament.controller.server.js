6f5f5551567ec8ef9104498d32d2137d2d9e0fd3
Junesik (Tony) Han <jhan80@my.centennialcollege.ca>
2022-12-05 22:23:33 -0500

    Edit teams

--- app/controllers/tournament.controller.server.js
+++ app/controllers/tournament.controller.server.js
@@ -52,22 +52,7 @@ export function ProcessTournamentAddPage(req, res, next){
             console.error(err);
             res.end(err);
         };
-        // let teams = Tournament.teams.split(",");
-        // for (let index = 0; index < teams.length; ++index){
-        //     const teamName = teams[index];
-        //     let newTeam = teamModel({
-        //         name: teamName,
-        //         teamNumber: index + 1,
-        //         tournamentID: Tournament._id,
-        //         score: 0
-        //     });
-        //     teamModel.create(newTeam, (err, team) => {
-        //         if (err){
-        //             console.error(err);
-        //             res.end(err);
-        //         }
-        //     })
-        // }
+ 
         let team1 = teamModel({
             name: req.body.team1,
             teamNumber: 1,
@@ -293,13 +278,6 @@ export function DisplayTournamentEditPage(req, res, next){
 
 export function ProcessTournamentEditPage(req, res, next){
     let id = req.params.id;
-
-    teamModel.remove({tournamentID: id}, (err) => {
-        if (err){
-            console.error(err);
-            res.end(err);
-        }
-    })
     
     let newTournament = tournamentModel({
         _id: req.body.id,
@@ -325,231 +303,135 @@ export function ProcessTournamentEditPage(req, res, next){
         team16: req.body.team16,
     });
 
-    // let teams = req.body.teams.split(",");
-    //     for (let index = 0; index < teams.length; ++index){
-    //         const teamName = teams[index];
-    //         let newTeam = teamModel({
-    //             name: teamName,
-    //             teamNumber: index + 1,
-    //             tournamentID: req.body.id,
-    //             score: 0
-    //         });
-    //         teamModel.create(newTeam, (err, team) => {
-    //             if (err){
-    //                 console.error(err);
-    //                 res.end(err);
-    //             }
-    //         })
-    //     }
-    let team1 = teamModel({
-        name: req.body.team1,
-        teamNumber: 1,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team1, (err, team) => {
+
+    let team1 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 1}, {name: req.body.team1});
+    teamModel.updateOne({tournamentID: id, teamNumber: 1}, team1, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team2 = teamModel({
-        name: req.body.team2,
-        teamNumber: 2,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team2, (err, team) => {
+    let team2 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 2}, {name: req.body.team2});
+    teamModel.updateOne({tournamentID: id, teamNumber: 2}, team2, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team3 = teamModel({
-        name: req.body.team3,
-        teamNumber: 3,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team3, (err, team) => {
+    let team3 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 3}, {name: req.body.team3});
+    teamModel.updateOne({tournamentID: id, teamNumber: 3}, team3, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team4 = teamModel({
-        name: req.body.team4,
-        teamNumber: 4,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team4, (err, team) => {
+    let team4 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 4}, {name: req.body.team4});
+    teamModel.updateOne({tournamentID: id, teamNumber: 4}, team4, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team5 = teamModel({
-        name: req.body.team5,
-        teamNumber: 5,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team5, (err, team) => {
+    let team5 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 5}, {name: req.body.team5});
+    teamModel.updateOne({tournamentID: id, teamNumber: 5}, team5, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team6 = teamModel({
-        name: req.body.team6,
-        teamNumber: 6,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team6, (err, team) => {
+    let team6 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 6}, {name: req.body.team6});
+    teamModel.updateOne({tournamentID: id, teamNumber: 6}, team6, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team7 = teamModel({
-        name: req.body.team7,
-        teamNumber: 7,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team7, (err, team) => {
+    let team7 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 7}, {name: req.body.team7});
+    teamModel.updateOne({tournamentID: id, teamNumber: 7}, team7, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team8 = teamModel({
-        name: req.body.team8,
-        teamNumber: 8,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team8, (err, team) => {
+    let team8 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 8}, {name: req.body.team8});
+    teamModel.updateOne({tournamentID: id, teamNumber: 8}, team8, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team9 = teamModel({
-        name: req.body.team9,
-        teamNumber: 9,
-        tournamentID:req.body.id,
-        score: 0
-    });
-    teamModel.create(team9, (err, team) => {
+    let team9 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 9}, {name: req.body.team9});
+    teamModel.updateOne({tournamentID: id, teamNumber: 9}, team9, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team10 = teamModel({
-        name: req.body.team10,
-        teamNumber: 10,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team10, (err, team) => {
+    let team10 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 10}, {name: req.body.team10});
+    teamModel.updateOne({tournamentID: id, teamNumber: 10}, team10, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team11 = teamModel({
-        name: req.body.team11,
-        teamNumber: 11,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team11, (err, team) => {
+    let team11 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 11}, {name: req.body.team11});
+    teamModel.updateOne({tournamentID: id, teamNumber: 11}, team11, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team12 = teamModel({
-        name: req.body.team12,
-        teamNumber: 12,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team12, (err, team) => {
+    let team12 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 12}, {name: req.body.team12});
+    teamModel.updateOne({tournamentID: id, teamNumber: 12}, team12, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team13 = teamModel({
-        name: req.body.team13,
-        teamNumber: 13,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team13, (err, team) => {
+    let team13 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 13}, {name: req.body.team13});
+    teamModel.updateOne({tournamentID: id, teamNumber: 13}, team13, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team14 = teamModel({
-        name: req.body.team14,
-        teamNumber: 14,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team14, (err, team) => {
+    let team14 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 14}, {name: req.body.team14});
+    teamModel.updateOne({tournamentID: id, teamNumber: 14}, team14, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team15 = teamModel({
-        name: req.body.team15,
-        teamNumber: 15,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team15, (err, team) => {
+    let team15 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 15}, {name: req.body.team15});
+    teamModel.updateOne({tournamentID: id, teamNumber: 15}, team15, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
+        };
+    })
 
-    let team16 = teamModel({
-        name: req.body.team16,
-        teamNumber: 16,
-        tournamentID: req.body.id,
-        score: 0
-    });
-    teamModel.create(team16, (err, team) => {
+    let team16 = teamModel.findOneAndUpdate({tournamentID: id, teamNumber: 16}, {name: req.body.team16});
+    teamModel.updateOne({tournamentID: id, teamNumber: 16}, team16, (err, Team) => {
         if (err){
             console.error(err);
             res.end(err);
-        }
-    });
-
-
+        };
+    })
+   
     tournamentModel.updateOne({_id: id}, newTournament, (err, Tournament) => {
         if (err){
             console.error(err);
@@ -557,9 +439,6 @@ export function ProcessTournamentEditPage(req, res, next){
         };
         res.redirect('/tournament-list');
     })
-
-
-
 }
 
 export function ProcessTournamentDelete(req, res, next){
